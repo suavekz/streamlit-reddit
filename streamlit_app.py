b972367639d98849c58839153d1e4288d92db124
Suavek Zajac <suavekzajac@ltytpxlgh0.home>
2023-08-30 14:32:33 +0200

    read and write to firestore securely

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,31 @@
 import streamlit as st
+from google.cloud import firestore
+from google.oauth2 import service_account
 
-st.header('Hello 🌎!')
-if st.button('Balloons?'):
-    st.balloons()
+import json
+key_dict = json.loads(st.secrets["textkey"])
+creds = service_account.Credentials.from_service_account_info(key_dict)
+db = firestore.Client(credentials=creds, project="streamlit-reddit-a5dbc")
+
+# Streamlit widgets to let a user create a new post
+title = st.text_input("Post title")
+url = st.text_input("Post url")
+submit = st.button("Submit new post")
+
+# Once the user has submitted, upload it to the database
+if title and url and submit:
+	doc_ref = db.collection("posts").document(title)
+	doc_ref.set({
+		"title": title,
+		"url": url
+	})
+
+# And then render each post, using some light Markdown
+posts_ref = db.collection("posts")
+for doc in posts_ref.stream():
+	post = doc.to_dict()
+	title = post["title"]
+	url = post["url"]
+
+	st.subheader(f"Post: {title}")
+	st.write(f":link: [{url}]({url})")
